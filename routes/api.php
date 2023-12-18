094de787fc055cfe33ddf3958019acf1c153f6b8
fsalles2022 <fabiolfs761@gmil.com>
2023-12-18 19:34:27 -0300

    Implemnetada as rotas ApiResources,cliente,carro,locacao,marca e modelo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\CarmodelController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::ApiResources(
+[
+    'cliente' => ClientController::class,
+    'carro' => CarController::class,
+    'locacao' => LocationController::class,
+    'marca' => BrandController::class,
+    'modelo' => CarmodelController::class,
+]
+);
