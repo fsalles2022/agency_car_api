8c51a43337ab734b16f152c8791ed73ed5ab53ba
fsalles2022 <fabiolfs761@gmil.com>
2023-12-27 11:20:55 -0300

    feat - Implementando failedValidation

--- app/Http/Requests/UpdateBrandRequest.php
+++ app/Http/Requests/UpdateBrandRequest.php
@@ -3,36 +3,28 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UpdateBrandRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            'name' => 'required|unique:brands,name,|min:3', 
-            'image' => 'required',
-        ];
+            'name' => 'unique:brands|min:3|max:255',
+                 ];
     }
-      public function messages(){
-        return  
-         [
-            'required' => 'O campo :attribute é obrigatório',
-            'name.unique' => 'O nome da marca já existe',
-            'name.min' => 'O nome deve ter no minimo 3 caracteres',
-         ];
-        
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Erro de validação',
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
