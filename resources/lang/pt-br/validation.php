f1be1cb579ec0e3925e956aff46dc74faeaf2119
fsalles2022 <fabiolfs761@gmil.com>
2023-12-27 19:29:32 -0300

    feat - Implementada a validação para o required dos campos name e image

--- resources/lang/pt-br/validation.php
+++ resources/lang/pt-br/validation.php
@@ -3,10 +3,12 @@
 return [
     'custom' => [
         'name' => [
+            'required' => "O campo nome deve ser preenchido corretamente",
             'min' => 'O campo nome deve ter pelo menos 3 caracteres.',
             'unique' => 'O nome da marca já existe',
         ],
         'image' => [
+            'required' => 'O campo imagem deve conter uma imagem válida',
             'mimes' =>"Somente arquivos com extensão .png",
         ],
     ],
